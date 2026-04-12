483578cf83a4f08f31665a54c31008526c3cb8e6
Mriidul Saxena <mriidulsaxena@gmail.com>
2026-04-12 21:39:47 +0000

    Added script

---
+++ first-shell-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# print the odd number
+echo 1,3,5,7,9
+# print the disk space
+du -h
+# to enable the debug mode
+set -x
+# print the current date and time
+date
+# to check the running processes
+top 
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function say hello() {
     console.log("Hello, world!");
+} 
+function say hi() {
+    console.log("Hi, world!");
 }
